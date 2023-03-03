a9dae3b568807f6e6d660388f0ae89f75a5a498c
Ben <66697315+Boxit379@users.noreply.github.com>
2023-03-02 19:13:24 -0700

    owo

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-a = 0;
-b = 1;
-function buttonClick() {
-    a += b;
-    document.getElementById("aCount").innerHTML = "a = " + a;
+score = 0;
+scoreIncrease = 1;
+function increaseScore() {
+    score += scoreIncrease;
+    document.getElementById("scoreCount").innerHTML = "var score = " + score + ";";
 }
